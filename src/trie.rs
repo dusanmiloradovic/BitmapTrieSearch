a076a1d61aed97eb4131c96cd1bee2b7d4999a57
mildusan <dusan@neurotone.com>
2025-12-24 16:10:37 +0100

    first commit

---
+++ src/trie.rs
@@ -0,0 +1,13 @@
+struct NodeIndex{
+    index: u32, //0 termination
+    restOfWord: Option<String>,// only for terminated
+    dictionaryIndex: u32, // 0 for no index -> if terminated, this is a link to an actual dictionary entry
+}
+
+// TODO add types of dictionary entries
+struct Trie {
+    bitmap: u64,
+    positions: Vec<NodeIndex>
+}
+
+impl
