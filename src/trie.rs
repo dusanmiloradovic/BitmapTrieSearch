0f65db4824c55f1b2b691458d989650f7cd216cf
Dusan Miloradovic <dusan.miloradovic@gmail.com>
2025-12-24 16:32:37 +0100

    logic for inserting and getting with count_ones

--- src/trie.rs
+++ src/trie.rs
@@ -10,3 +10,30 @@ struct Trie {
     positions: Vec<NodeIndex>
 }
 
+impl Trie {
+    pub fn insert_at(&mut self, bit_pos: u32, node: NodeIndex) {
+        let mask = (1u64 << bit_pos) - 1;
+        // The index in the Vec is the number of set bits to the right of bit_pos
+        let array_idx = (self.bitmap & mask).count_ones() as usize;
+
+        if (self.bitmap & (1u64 << bit_pos)) == 0 {
+            // Bit was not set: insert new entry into the vector
+            self.bitmap |= 1u64 << bit_pos;
+            self.positions.insert(array_idx, node);
+        } else {
+            // Bit was already set: update existing entry
+            self.positions[array_idx] = node;
+        }
+    }
+
+    pub fn get(&self, bit_pos: u32) -> Option<&NodeIndex> {
+        if (self.bitmap & (1u64 << bit_pos)) != 0 {
+            let mask = (1u64 << bit_pos) - 1;
+            let array_idx = (self.bitmap & mask).count_ones() as usize;
+            Some(&self.positions[array_idx])
+        } else {
+            None
+        }
+    }
+}
+
