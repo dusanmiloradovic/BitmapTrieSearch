6fd6b3d58b299413b9af1729572ac26de4412ec1
Dusan Miloradovic <dusan.miloradovic@gmail.com>
2026-01-13 17:43:27 +0000

    borrow checker struggle

--- src/listener.rs
+++ src/listener.rs
@@ -1,27 +1,37 @@
+use crate::command::Command;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
+use std::thread;
+use serde::Deserialize;
 
-pub fn listen () ->  std::io::Result<()>{
+pub fn listen() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4444")?;
 
     let trieMap = crate::access::TrieMap::new();
     let arcTrieMap = std::sync::Arc::new(trieMap);
 
     // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?, &arcTrieMap);
+    for res_stream in listener.incoming() {
+        let arc_clone = Arc::clone(&arcTrieMap);
+        thread::spawn(move || {
+            match res_stream {
+                Ok(stream) => handle_client(stream, &arc_clone),
+                Err(e) => panic!("{:?}", e),
+            }
+        });
     }
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream, arcTrieMap: &std::sync::Arc<crate::access::TrieMap>) {
-    let a = Arc::clone(&arcTrieMap);
-    let mut str= String::new();
-    let mut reader = BufReader::new(&mut stream);
-    reader.read_line(&mut str);
-    //stream.read_to_string(&mut str).expect("TODO: panic message");
-    println!("Received: {}", str);
-    write!(stream,"hello {}", str).expect("TODO panic message");
-    stream.flush().expect("TODO panic message");
-}
+fn handle_client(stream: TcpStream, cloned_arc: &std::sync::Arc<crate::access::TrieMap>) {
+        let reader = BufReader::new(&stream);
+        let mut deserializer = serde_json::Deserializer::from_reader(reader);
+
+        // This allows handling multiple commands sent over the same connection
+        while let Ok(command) = Command::deserialize(&mut deserializer) {
+            println!("Received: {:?}", command);
+            // ... process command ...
+            write!(&stream, "Success\n").expect("Failed to write");
+        }
+    }
